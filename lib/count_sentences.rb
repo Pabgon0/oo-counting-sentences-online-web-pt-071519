966e09e3f568af985dafb1adb0e662015e3cf5da
Pablo Gonzalez <pabgon0@gmail.com>
2019-08-02 19:26:07 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,18 @@ class String
   end
 
   def question?
-    self.end_with("?")
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.end_with?(".", "?", "!")
+      self.split.count
+    else
+      return 0
+    end
   end
 end
